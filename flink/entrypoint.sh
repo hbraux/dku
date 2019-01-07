785dd3c7e14bff5e534b8ff057c92396af488e1b
Harold Braux <harold.braux@email.com>
2019-01-07 16:14:33 +0000

    minor change

--- flink/entrypoint.sh
+++ flink/entrypoint.sh
@@ -2,9 +2,10 @@
 
 function _setup {
   [[ -f .setup ]] && return
-  sed -i -e 's/^#jobmanager.web.address:.*/jobmanager.web.address: 0.0.0.0/' conf/flink-conf.yaml
+  sed -i 's/^#jobmanager.web.address:.*/jobmanager.web.address: 0.0.0.0/' conf/flink-conf.yaml
+  sed -i 's~^# io.tmp.dirs: /tmp*~io.tmp.dirs: /data~' conf/flink-conf.yaml
   if [[ $HEAP == low ]]; then 
-    sed -i -e 's/heap.size: 1024m/heap.size: 256m/g' conf/flink-conf.yaml
+    sed -i 's/heap.size: 1024m/heap.size: 256m/g' conf/flink-conf.yaml
   fi
   touch .setup
 }
