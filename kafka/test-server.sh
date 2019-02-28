8fc5ae4e50f4999b8e738eff8b44fbf0a86dcab8
haroldbraux <harold.braux@bleckwen.ai>
2019-02-28 15:06:29 +0100

    wip

--- kafka/test-server.sh
+++ kafka/test-server.sh
@@ -1,4 +1,5 @@
 dockerRun kafka-topics.sh --zookeeper kafka:2181 --create --topic mytopic --partitions 1 --replication-factor 1
-dockerRun kafka-json.sh kafka:9092 mytopic uuid:%uuid date=%now val=%rand
+dockerRun kafka-json.py --count 1 kafka:9092 mytopic '{"uuid":"%UUID","key":"STR3","val":%INT99}'
+
 
 
